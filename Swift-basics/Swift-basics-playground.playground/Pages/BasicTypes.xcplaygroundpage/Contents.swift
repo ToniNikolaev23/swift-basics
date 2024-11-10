44296c4dcdf29e8c1445b36acd6111b5c327d474
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-11-10 21:10:31 +0200

    Types and Basics

--- Swift-basics/Swift-basics-playground.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
+++ Swift-basics/Swift-basics-playground.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// String is "regular text"
+let myFirstItem: String = "Hello, world!"
+
+// Reference previously created objects
+let myTitle = myFirstItem
+
+// Boolean
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// Swift is a type-safe language
+let myFourthItem: Bool = true
+
+// Date
+let myFirstDate: Date = Date()
+
+// Numbers - can be Int, Double, CGFloat & more
+let myFirstNumber: Int = 1
+// Use Double for MATH
+let mySecondNumber: Double = 1.0
+// Use CGFloat for UI
+let myThirdNumber: CGFloat = 1.0
--- Swift-basics/Swift-basics-playground.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ Swift-basics/Swift-basics-playground.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -1,3 +1,24 @@
 import Foundation
 
 var greeting = "Hello, playground"
+
+print(greeting)
+
+// This is a single-line comment
+
+/*
+ Multi-line comment
+ that goes to
+ multiple lines!
+ */
+
+// Naming Conventions
+
+// Camel Case
+let firstGreeting = "Hello, world!"
+
+// Snake Case
+let this_is_what_snake_case_looks_like = "Hello, world!"
+
+//Pascal Case
+let ThisIsWhatPascalCaseLooksLike = "Hello, world!"
